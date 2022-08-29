44da48e598185fe5f3f5d627db829682736ad561
Jonathan Wong <jonathanfwong@gmail.com>
2022-08-29 18:58:16 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/Nav";
+import Landing from "./components/Landing";
+import Highlights from "./components/Highlights";
+import Featured from "./components/Featured";
+import Discounted from "./components/Discounted";
+import Explore from "./components/Explore";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data"
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, {...book, quantity: 1}])
+  }
+
+  function removeFromCart(book) {
+    setCart(cart.filter(item => item.id !== book.id))
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter;
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map(item => (item.id === book.id) ? ({...item, quantity: quantity}) : item)
+    )
+  }
+
+  // useEffect(() => {
+  //   console.log(cart)
+  // }, [cart])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav numberOfItems={numberOfItems()} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/books' element={<Books books={books} />} />
+          <Route path='/books/:id' element={(
+            <BookInfo 
+              books={books}
+              addToCart={addToCart}
+              cart={cart}
+            />
+          )} /> 
+          <Route path='/cart' element={(
+            <Cart 
+              books={books}
+              cart={cart}
+              removeFromCart={removeFromCart}
+              changeQuantity={changeQuantity}
+            />
+          )} />   
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Discounted.jsx
@@ -0,0 +1,26 @@
+import { books } from "../data"
+import Book from "./ui/Book"
+
+function Discounted() {
+  return (
+    <section id="recent">
+      <div className="container">
+        <div className="row">
+          <div className="section__title">
+            Discounted <span className="purple">Books</span>
+          </div>
+          <div className="books">
+            {
+              books
+                .filter(book => book.salePrice > 0)
+                .slice(0, 8)
+                .map(book => <Book book={book} key={book.id} />)
+                
+            }
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default Discounted
